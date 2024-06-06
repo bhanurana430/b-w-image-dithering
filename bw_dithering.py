d8b7ab55049dc16302e1eba3c6493477c98c723f
Bhanu <bhanurana430@gmail.com>
2024-06-06 15:33:21 +0200

    Basic window initialization

---
+++ bw_dithering.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+from tkinter import ttk
+from PIL import Image, ImageTk
+import numpy as np
+import threading
+
+root = tk.Tk()
+root.title("B&W Dithering Editor")
+
+root.mainloop()
